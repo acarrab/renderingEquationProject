f383debdce56f9e295e2f929b213bacfb8fabafd
Angelo P Carrabba Jr <acarrab@titan5.cs.clemson.edu>
2017-04-20 14:40:37 -0400

    removing things

--- lib/winData.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/winData.h"
-WinData::WinData() :
-  d(Data::getInstance()),
-  coords({d.getXmlInt("window/x"),
-	d.getXmlInt("window/y"),
-	d.getXmlInt("window/width"),
-	d.getXmlInt("window/height")}),
-  title(d.getXmlStr("window/title"))
-{}
